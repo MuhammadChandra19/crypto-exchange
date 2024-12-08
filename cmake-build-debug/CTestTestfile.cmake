b6343b1840abe718058585dadffa02eb6b8bb1bb
Muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2024-12-08 21:12:45 +0700

    feat: ignore debug

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/muhammadchandrazulfikar/Documents/cpp-playground/crypto-exchange
-# Build directory: /Users/muhammadchandrazulfikar/Documents/cpp-playground/crypto-exchange/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/fmt-build")
-subdirs("src")
-subdirs("tests")
--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/muhammadchandrazulfikar/Documents/cpp-playground/crypto-exchange/tests
-# Build directory: /Users/muhammadchandrazulfikar/Documents/cpp-playground/crypto-exchange/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[CryptoExchangeTests]=] "/Users/muhammadchandrazulfikar/Documents/cpp-playground/crypto-exchange/cmake-build-debug/tests/tests")
-set_tests_properties([=[CryptoExchangeTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/muhammadchandrazulfikar/Documents/cpp-playground/crypto-exchange/tests/CMakeLists.txt;28;add_test;/Users/muhammadchandrazulfikar/Documents/cpp-playground/crypto-exchange/tests/CMakeLists.txt;0;")
-subdirs("../_deps/googletest-build")
