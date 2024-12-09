63de23cd4c473654991d18f4a15274d3b9cb77d3
Muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2024-12-09 15:44:50 +0700

    feat: implement matching engine -> place order

--- src/matching_engine/MatchingEngine.cpp
+++ src/matching_engine/MatchingEngine.cpp
@@ -1,5 +1,9 @@
 #include "MatchingEngine.hpp"
 
+#include <iostream>
+
+#include "Order.hpp"
+
 MatchingEngine::MatchingEngine() {
     std::cout << "MatchingEngine Initialized." << '\n';
 }
@@ -12,6 +16,29 @@ void MatchingEngine::initialize() {
     std::cout << "MatchingEngine is now ready." << '\n';
 }
 
-void MatchingEngine::processOrder(const std::string& order) {
-    std::cout << "Processing order: " << order << '\n';
+int64_t MatchingEngine::handlePlaceOrder(const PlaceOrderRequest &placeOrderRequest) {
+    const auto limit = orderbook.FindOrCreateLimit(placeOrderRequest.price, placeOrderRequest.bid);
+    auto const order = std::make_shared<Order>(
+        placeOrderRequest.id,
+        placeOrderRequest.userId,
+        placeOrderRequest.size,
+        placeOrderRequest.bid,
+        limit,
+        std::chrono::duration_cast<std::chrono::milliseconds>(
+                                    std::chrono::system_clock::now().time_since_epoch())
+                                    .count()
+        );
+    if (placeOrderRequest.orderType == 0) { // limit orders
+        orderbook.PlaceLimitOrder(placeOrderRequest.price,order);
+        return placeOrderRequest.id;
+    }
+
+    if (placeOrderRequest.orderType == 1) {
+        orderbook.PlaceMarketOrder(order);
+        return placeOrderRequest.id;
+    }
+
+    return 0;
 }
+
+
--- src/matching_engine/MatchingEngine.hpp
+++ src/matching_engine/MatchingEngine.hpp
@@ -1,13 +1,48 @@
 #pragma once
 
 #include <string>
-#include <iostream>
+#include <vector>
+
+#include "OrderBook.hpp"
+
+struct LiveOrder
+{
+    int64_t id;
+    int64_t userId;
+    double size;
+    bool bid;
+    int64_t timestamp;
+};
+
+struct PlaceOrderRequest {
+    int64_t id;
+    int64_t userId;
+    int orderType;
+    bool bid;
+    double size;
+    double price;
+    std::string pair;
+};
+
+struct  OrderBookData
+{
+    double totalBidVolume;
+    double totalAskVolume;
+    std::vector<std::shared_ptr<LiveOrder>> asks;
+    std::vector<std::shared_ptr<LiveOrder>> bids;
+};
+
+
 
 class MatchingEngine {
 public:
+    OrderBook orderbook;
     MatchingEngine();
     ~MatchingEngine();
 
     static void initialize();
-    static void processOrder(const std::string& order);
+
+    int64_t handlePlaceOrder(const PlaceOrderRequest& placeOrderRequest);
+    OrderBookData getOrderBookData();
+    void handleCancelOrder(std::string id);
 };
